ca9cdd84b0c8585fa0ac78d5defd56179e012147
Henry Romp <henry@tb.deb.net>
2017-09-21 09:38:13 -0400

    update timeupdate.bash

--- timeupdate.bash
+++ timeupdate.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-DISPLAY=:0
+export DISPLAY=:0
 hour=$(date -d "7 hours ago" +%H)
 min=$(date -d "7 hours ago" +%M)
  
 if [[ ! $((${min}%15)) -eq 0 ]]; then
-        min=$((${min}-$((${min}%15))))
+      min=$((${min}-$((${min}%15))))
 fi
 
-if $min==0 then;
-	$min="00"
+if [[ $min == 0 ]]; then
+      min="00";
 fi
 
 feh --image-bg white --bg-center "/home/username/randall-clock-desktop-background/now/${hour}h${min}m.png"
